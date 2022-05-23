c030031de65d3af4c804e4a1833fe99229b56bc7
Koohi45 <haein0405@ewhain.net>
2022-05-23 20:31:07 +0900

    mesh 정보 불러오기

--- Assets/Scripts/ApplyMesh.cs
+++ Assets/Scripts/ApplyMesh.cs
@@ -8,26 +8,25 @@ public class ApplyMesh : MonoBehaviour
     PhotonView photonview;
     PlayerInfo info;
 
-    //mesh
-    public SkinnedMeshRenderer mesh_eye;
-    public SkinnedMeshRenderer mesh_hair_front;
-    public SkinnedMeshRenderer mesh_hair_back;
-    public SkinnedMeshRenderer mesh_top;
-    public SkinnedMeshRenderer mesh_bottom;
-    public SkinnedMeshRenderer mesh_shoes;
-    public SkinnedMeshRenderer mesh_acc;
+    [Header("Avatar Mesh")]
+    [SerializeField] private SkinnedMeshRenderer mesh_eye;
+    [SerializeField] private SkinnedMeshRenderer mesh_hair_front;
+    [SerializeField] private SkinnedMeshRenderer mesh_hair_back;
+    //[SerializeField] private SkinnedMeshRenderer mesh_mouse;
+    [SerializeField] private SkinnedMeshRenderer mesh_top;
+    [SerializeField] private SkinnedMeshRenderer mesh_bottom;
+    [SerializeField] private SkinnedMeshRenderer mesh_shoes;
+    //[SerializeField] private SkinnedMeshRenderer mesh_acc;
 
-    int front_idx;
-    int back_idx;
-
-    [Header("Avatar")]
+    [Header("Mesh")]
     [SerializeField] private Mesh[] eye;
     [SerializeField] private Mesh[] hair_front;
     [SerializeField] private Mesh[] hair_back;
+    //[SerializeField] private Mesh[] mouse;
     [SerializeField] private Mesh[] top;
     [SerializeField] private Mesh[] bottom;
     [SerializeField] private Mesh[] shoes;
-    [SerializeField] private Mesh[] acc;
+    //[SerializeField] private Mesh[] acc;
 
 
 
@@ -35,6 +34,7 @@ public class ApplyMesh : MonoBehaviour
     {
         photonview = PhotonView.Get(this);
         info = GameObject.Find("PlayerInfo").GetComponent<PlayerInfo>();
+        info.ReadAvatarInfo();
         Avatar();
     }
 
@@ -43,15 +43,41 @@ public class ApplyMesh : MonoBehaviour
         if (!photonview.IsMine)
             return;
 
-        (front_idx, back_idx) = info.Get();
-        Debug.Log((front_idx, back_idx));
-        photonview.RPC("Apply", RpcTarget.AllBuffered, front_idx, back_idx);
+        AvatarInfo avatar = info.avatarinfo;
+        photonview.RPC("Apply", RpcTarget.AllBuffered, avatar);
+        
+        //(front_idx, back_idx) = info.Get();
+        //Debug.Log((front_idx, back_idx));
+        //photonview.RPC("Apply", RpcTarget.AllBuffered, front_idx, back_idx);
     }
 
     [PunRPC]
-    void Apply(int f, int b)
+    void Apply(AvatarInfo avatar)
     {
-        mesh_hair_front.sharedMesh = hair_front[f];
-        mesh_hair_back.sharedMesh = hair_back[b];
+        //ÇÇºÎ»ö
+
+        //´«
+        mesh_eye.sharedMesh = eye[avatar.eye.type];
+        //»ö
+
+        //ÀÔ
+        //mesh_mouse.sharedMesh = mouse[avatar.mouse.type];
+
+        //¸Ó¸®
+        mesh_hair_front.sharedMesh = hair_front[avatar.hair.front_type];
+        mesh_hair_back.sharedMesh = hair_back[avatar.hair.back_type];
+
+        //¿Ê
+        mesh_top.sharedMesh = top[avatar.cloth.top];
+        mesh_bottom.sharedMesh = top[avatar.cloth.bottom];
+        mesh_shoes.sharedMesh = top[avatar.cloth.shoes];
+        //mesh_acc.sharedMesh = top[avatar.cloth.acc];
+
+        Debug.Log(avatar.eye.type);
+        Debug.Log(avatar.mouse.type);
+        Debug.Log(avatar.hair.front_type);
+        Debug.Log(avatar.hair.back_type);
+        Debug.Log(avatar.cloth.top);
+        Debug.Log(avatar.cloth.bottom);
     }
 }
